0791dcea6cb90412ac71733380ad88c78d4685d3
kashyapsavaliya <kkashyap.savaliya@gmail.com>
2019-09-08 20:52:34 -0400

    Finish keylistener

--- src/com/kashyapsavaliya/chip8/Keyboard.java
+++ src/com/kashyapsavaliya/chip8/Keyboard.java
@@ -2,15 +2,35 @@ package com.kashyapsavaliya.chip8;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.HashMap;
 
 public class Keyboard implements KeyListener {
 
-    public Keyboard() {
+    private HashMap<Character, Integer> key;
+    private static int[] keyBuffer;
 
+    public Keyboard() {
+        key = new HashMap<>();
+        keyBuffer = new int[16];
     }
 
     public void setKeys() {
-
+        key.put('1', 1);
+        key.put('2', 2);
+        key.put('3', 3);
+        key.put('q', 4);
+        key.put('w', 5);
+        key.put('e', 6);
+        key.put('a', 7);
+        key.put('s', 8);
+        key.put('d', 9);
+        key.put('x', 0);
+        key.put('z', 0xA);
+        key.put('c', 0xB);
+        key.put('4', 0xC);
+        key.put('r', 0xD);
+        key.put('f', 0xE);
+        key.put('v', 0xF);
     }
 
     @Override
@@ -20,11 +40,20 @@ public class Keyboard implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-
+        if (key.get(e.getKeyCode()) != -1) {
+            keyBuffer[key.get(e.getKeyCode())] = 1;
+        }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-
+        if (key.get(e.getKeyCode()) != -1) {
+            keyBuffer[key.get(e.getKeyCode())] = 1;
+        }
     }
+
+    public static int[] getKeyBuffer() {
+        return keyBuffer;
+    }
+
 }
